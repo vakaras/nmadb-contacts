ded69a2d74d62294cd980b5704a0452209392e6f
Vytautas Astrauskas <vastrauskas@gmail.com>
2012-01-28 19:22:00 +0200

    Improved admin.

--- src/nmadb_contacts/admin.py
+++ src/nmadb_contacts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
 
 from nmadb_contacts import models
 from nmadb_contacts import forms
@@ -25,6 +26,51 @@ class MunicipalityAdmin(admin.ModelAdmin):
             )
 
 
+class EmailInline(admin.TabularInline):
+    """ Inline email administration.
+    """
+
+    model = models.Email
+
+    extra = 0
+
+
+class PhoneInline(admin.TabularInline):
+    """ Inline phone administration.
+    """
+
+    model = models.Phone
+
+    extra = 0
+
+
+class AddressInline(admin.TabularInline):
+    """ Inline address administration.
+    """
+
+    model = models.Address
+
+    extra = 0
+
+
+class InstitutionInline(admin.TabularInline):
+    """ Inline institution administration.
+    """
+
+    model = models.Institution
+
+    extra = 0
+
+
+class InfoForContractsInline(admin.StackedInline):
+    """ Inline information for contracts administration.
+    """
+
+    model = models.InfoForContracts
+
+    extra = 0
+
+
 class HumanAdmin(admin.ModelAdmin):
     """ Administration for human.
     """
@@ -36,6 +82,10 @@ class HumanAdmin(admin.ModelAdmin):
             'first_name',
             'last_name',
             'birth_date',
+            'get_address',
+            'get_phones',
+            'get_emails',
+            'has_contracts_information',
             )
 
     list_filter = [
@@ -46,6 +96,54 @@ class HumanAdmin(admin.ModelAdmin):
             'last_name',
             'old_last_name',
             )
+
+    inlines = [
+            EmailInline,
+            PhoneInline,
+            AddressInline,
+            InfoForContractsInline,
+            ]
+
+    def get_address(self, obj):
+        """ Returns main address, address column value.
+        """
+
+        if obj.main_address:
+            return obj.main_address.address
+        else:
+            return u''
+    get_address.short_description = _("Main address")
+
+    def get_phones(self, obj):
+        """ Returns concatenation of all used phone numbers.
+        """
+
+        return u'; '.join([
+            phone.number
+            for phone in obj.phone_set.exclude(used=False)
+            ])
+    get_phones.short_description = _("Phone numbers")
+
+    def get_emails(self, obj):
+        """ Returns concatenation of all used emails.
+        """
+
+        return u'; '.join([
+            email.address
+            for email in obj.email_set.exclude(used=False)
+            ])
+    get_emails.short_description = _("Email addresses")
+
+    def has_contracts_information(self, obj):
+        """ Returns if this human has contract information.
+        """
+
+        try:
+            return obj.infoforcontracts and True
+        except models.InfoForContracts.DoesNotExist:
+            return False
+    has_contracts_information.short_description = _("Contract info")
+    has_contracts_information.boolean = True
 
 
 admin.site.register(models.Human, HumanAdmin)
